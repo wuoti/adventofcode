84adb0fffcd037ad564fd9de4b914ff3b83fe5ac
Kalle Wuoti <kalle.wuoti@reaktor.com>
2015-12-04 11:05:48 +0200

    Use streams on day 1 exercise

--- day1/prog.scala
+++ day1/prog.scala
@@ -1,20 +1,17 @@
 import scala.io.Source
 
-val pars = Source.fromFile("input.txt").toList
+val pars = Source.fromFile("input.txt").toStream
 
 def upOrDown(c: Char) = if (c == '(') 1 else -1
 
 val finalFloor = pars.foldLeft(0)((floor, p) => floor + upOrDown(p))
 
-def foo(floor: Int, seq: Int, pars: List[Char]): Int = {
-  if (floor == -1) seq 
-  else pars match {
-    case h :: t => foo(floor + upOrDown(h), seq + 1, t)
-    case Nil => sys.error("Never reached basement")
-  }    
-}
-
-val firstTimeInBasement = foo(0, 0, pars)
+val firstTimeInBasement = pars
+  .map(upOrDown)
+  .scanLeft(0){ case (floor, d) => floor + d }
+  .zipWithIndex
+  .filter(_._1 == -1)
+  .head._2
 
 println(s"The instructions take santa to floor $finalFloor")
 println(s"The first time santa enters the basement is at instruction number $firstTimeInBasement")
